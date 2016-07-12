82d475ec06da26d0904552787256c84e9625f864
Fabri Di Napoli <fabrizio.dinapoli@scmspain.com>
2016-07-12 16:11:07 +0200

    Unit Test refactor [BLUE]

--- FizzBuzzKata/test/test_FizzBuzz.py
+++ FizzBuzzKata/test/test_FizzBuzz.py
@@ -4,16 +4,19 @@ from FizzBuzzKata.src.FizzBuzz import FizzBuzz
 
 class TestFizzBuzz(TestCase):
 
-    def test_should_print_fizz_on_multiples_of_three(self):
-        fizz_buzz = FizzBuzz()
+    FIZZ = "Fizz"
 
-        output = fizz_buzz.evaluate_number(3)
-        expected_output = "Fizz"
-        self.assertEqual(expected_output, output)
+    BUZZ = "Buzz"
+
+    __fizz_buzz = None
+
+    def setUp(self):
+        self.__fizz_buzz = FizzBuzz()
+
+    def test_should_print_fizz_on_multiples_of_three(self):
+        output = self.__fizz_buzz.evaluate_number(3)
+        self.assertEqual(self.FIZZ, output)
 
     def test_should_print_buzz_on_multiples_of_five(self):
-        fizz_buzz = FizzBuzz()
-
-        output = fizz_buzz.evaluate_number(5)
-        expected_output = "Buzz"
-        self.assertEqual(expected_output, output)
+        output = self.__fizz_buzz.evaluate_number(5)
+        self.assertEqual(self.BUZZ, output)
