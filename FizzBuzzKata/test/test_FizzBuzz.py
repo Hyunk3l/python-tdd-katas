2adb8061ffc0a7f514cf76e95bb4ae67ef2fb5b8
Fabri Di Napoli <fabrizio.dinapoli@scmspain.com>
2016-07-12 16:07:04 +0200

    Should print buzz on multiples of five [RED]

--- FizzBuzzKata/test/test_FizzBuzz.py
+++ FizzBuzzKata/test/test_FizzBuzz.py
@@ -11,3 +11,9 @@ class TestFizzBuzz(TestCase):
         expected_output = "Fizz"
         self.assertEqual(expected_output, output)
 
+    def test_should_print_buzz_on_multiples_of_five(self):
+        fizz_buzz = FizzBuzz()
+
+        output = fizz_buzz.evaluate_number(5)
+        expected_output = "Buzz"
+        self.assertEqual(expected_output, output)
