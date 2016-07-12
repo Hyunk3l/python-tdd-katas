43fb0c2ccf9c818caf6ae318841c6a2e74925c84
Fabri Di Napoli <fabrizio.dinapoli@scmspain.com>
2016-07-12 16:08:28 +0200

    Create code to pass should print buzz on multiples of five test case [GREEN]

--- FizzBuzzKata/src/FizzBuzz.py
+++ FizzBuzzKata/src/FizzBuzz.py
@@ -5,5 +5,7 @@ class FizzBuzz:
     def evaluate_number(self, number):
         if number % 3 == 0:
             return "Fizz"
+        if number % 5 == 0:
+            return "Buzz"
         else:
             return str(number)
