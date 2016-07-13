ad4d51e70944e17bf15f11ce3a9fe66562761363
Fabri Di Napoli <fabrizio.dinapoli@scmspain.com>
2016-07-13 15:06:29 +0200

    Refactor code [BLUE]

--- FizzBuzzKata/src/FizzBuzz.py
+++ FizzBuzzKata/src/FizzBuzz.py
@@ -5,13 +5,19 @@ class FizzBuzz:
 
     def evaluate_number(self, number):
         if self.__is_multiple_of_three_and_five(number):
-            return self.FIZZ+self.BUZZ
-        elif self.__is_multiple_of_three(number) | self.__has_number_a_three(number):
+            return self.FIZZ + self.BUZZ
+        elif self.__is_fuzzable(number):
             return self.FIZZ
-        elif self.__is_multiple_of_five(number) | self.__has_number_a_five(number):
+        elif self.__is_buzzable(number):
             return self.BUZZ
         else:
             return number
+
+    def __is_fuzzable(self, number):
+        return self.__is_multiple_of_three(number) | self.__has_number_a_three(number)
+
+    def __is_buzzable(self, number):
+        return self.__is_multiple_of_five(number) | self.__has_number_a_five(number)
 
     def __is_multiple_of_three_and_five(self, number):
         return self.__is_multiple_of_three(number) & self.__is_multiple_of_five(number)
