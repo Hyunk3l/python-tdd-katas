8154b01c14a2d56e9a280eed9fe3ae85f6d02d55
Fabri Di Napoli <fabrizio.dinapoli@scmspain.com>
2016-11-02 18:07:40 +0100

    [BLUE] Refactor should get perfect score test case

--- BowlingKata/src/Bowling.py
+++ BowlingKata/src/Bowling.py
@@ -1,13 +1,21 @@
 
 class Bowling:
 
-    __rolls = []
+    __rolls = None
+
+    def __init__(self):
+        self.__rolls = []
 
     def roll(self, score):
         self.__rolls.append(score)
 
     def getScore(self):
         totalScore = 0
-        for roll in self.__rolls:
-            totalScore += roll
+        for index, roll in enumerate(self.__rolls):
+            if index - 2 >= 0 and self.__rolls[index-2] == "X" and self.__rolls[index-1] == "X":
+                totalScore += 30
+            elif "X" == roll and index+1 <= len(self.__rolls) and self.__rolls[index+1] == "X":
+                continue
+            else:
+                totalScore += roll
         return totalScore
