6cf7135e35accd8d3af4a881fe4971ff266757a3
Fabri Di Napoli <fabrizio.dinapoli@scmspain.com>
2016-11-02 18:07:16 +0100

    [RED] Add should get perfect score test case

--- BowlingKata/test/test_Bowling.py
+++ BowlingKata/test/test_Bowling.py
@@ -1,24 +1,31 @@
-
 from unittest import TestCase
 from BowlingKata.src.Bowling import Bowling
 
+
 class TestBowling(TestCase):
 
-    def test_should_get_current_score(self):
-        bowling = Bowling()
-        bowling.roll(10)
-        self.assertEqual(10, bowling.getScore())
+    bowling = None
+
+    def setUp(self):
+        self.bowling = Bowling()
 
     def test_should_get_perfect_score(self):
-        bowling = Bowling()
-        bowling.roll(10)
-        bowling.roll(10)
-        bowling.roll(10)
-        bowling.roll(10)
-        bowling.roll(10)
-        bowling.roll(10)
-        bowling.roll(10)
-        bowling.roll(10)
-        bowling.roll(10)
-        bowling.roll(10)
-        self.assertEquals(300, bowling.getScore())
+
+        self.bowling.roll("X")
+        self.bowling.roll("X")
+        self.bowling.roll("X")
+        self.bowling.roll("X")
+        self.bowling.roll("X")
+        self.bowling.roll("X")
+        self.bowling.roll("X")
+        self.bowling.roll("X")
+        self.bowling.roll("X")
+        self.bowling.roll("X")
+        self.bowling.roll("X")
+        self.bowling.roll("X")
+
+        self.assertEqual(300, self.bowling.getScore())
+
+    def test_should_get_current_score(self):
+        self.bowling.roll(10)
+        self.assertEqual(10, self.bowling.getScore())
