0856e7b5e27e04ed0e690a082bc336da6b38808c
Fabri Di Napoli <fabrizio.dinapoli@scmspain.com>
2016-11-02 18:09:57 +0100

    [BLUE] Refactor should get perfect score test case

--- BowlingKata/src/Bowling.py
+++ BowlingKata/src/Bowling.py
@@ -9,13 +9,13 @@ class Bowling:
     def roll(self, score):
         self.__rolls.append(score)
 
-    def getScore(self):
-        totalScore = 0
+    def get_score(self):
+        total_score = 0
         for index, roll in enumerate(self.__rolls):
             if index - 2 >= 0 and self.__rolls[index-2] == "X" and self.__rolls[index-1] == "X":
-                totalScore += 30
+                total_score += 30
             elif "X" == roll and index+1 <= len(self.__rolls) and self.__rolls[index+1] == "X":
                 continue
             else:
-                totalScore += roll
-        return totalScore
+                total_score += roll
+        return total_score
--- BowlingKata/test/test_Bowling.py
+++ BowlingKata/test/test_Bowling.py
@@ -11,21 +11,11 @@ class TestBowling(TestCase):
 
     def test_should_get_perfect_score(self):
 
-        self.bowling.roll("X")
-        self.bowling.roll("X")
-        self.bowling.roll("X")
-        self.bowling.roll("X")
-        self.bowling.roll("X")
-        self.bowling.roll("X")
-        self.bowling.roll("X")
-        self.bowling.roll("X")
-        self.bowling.roll("X")
-        self.bowling.roll("X")
-        self.bowling.roll("X")
-        self.bowling.roll("X")
+        for current in range(0, 12):
+            self.bowling.roll("X")
 
-        self.assertEqual(300, self.bowling.getScore())
+        self.assertEqual(300, self.bowling.get_score())
 
     def test_should_get_current_score(self):
         self.bowling.roll(10)
-        self.assertEqual(10, self.bowling.getScore())
+        self.assertEqual(10, self.bowling.get_score())
