f9fdfd4d04fdfe50a99b0b1b27b6a06578662c08
Fabri Di Napoli <fabrizio.dinapoli@scmspain.com>
2016-11-03 17:19:37 +0100

    [BLUE] Refactor Bowling class

--- BowlingKata/src/Bowling.py
+++ BowlingKata/src/Bowling.py
@@ -16,10 +16,20 @@ class Bowling:
     def get_score(self):
         total_score = 0
         for index, roll in enumerate(self.__rolls):
-            if index - 2 >= 0 and self.__rolls[index-2] == self.STRIKE and self.__rolls[index-1] == self.STRIKE:
+            if self.__is_third_strike(index):
                 total_score += self.MULTIPLE_STRIKES_SCORE
-            elif self.STRIKE == roll and index+1 <= len(self.__rolls) and self.__rolls[index+1] == self.STRIKE:
+            elif self.STRIKE == roll and self.__is_next_roll_strike(index):
                 continue
             else:
                 total_score += roll
         return total_score
+
+    def __is_third_strike(self, index):
+        return (
+            index - 2 >= 0
+            and self.__rolls[index-2] == self.STRIKE
+            and self.__rolls[index-1] == self.STRIKE
+        )
+
+    def __is_next_roll_strike(self, index):
+        return index+1 <= len(self.__rolls) and self.__rolls[index+1] == self.STRIKE
