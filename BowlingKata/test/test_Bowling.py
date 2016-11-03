1e34b934123576023fd4f5da24dacedc14e846b5
Fabri Di Napoli <fabrizio.dinapoli@scmspain.com>
2016-11-03 17:28:23 +0100

    [GREEN] Add test case to get score without strikes

--- BowlingKata/test/test_Bowling.py
+++ BowlingKata/test/test_Bowling.py
@@ -14,13 +14,56 @@ class TestBowling(TestCase):
     def setUp(self):
         self.bowling = Bowling()
 
-    def test_should_get_perfect_score(self):
+    def test_should_get_current_score(self):
+        self.bowling.roll(self.STRIKE_POINTS)
+        self.assertEqual(self.STRIKE_POINTS, self.bowling.get_score())
 
+    def test_should_get_perfect_score(self):
         for current in range(0, 12):
             self.bowling.roll(self.STRIKE)
 
         self.assertEqual(self.PERFECT_SCORE, self.bowling.get_score())
 
-    def test_should_get_current_score(self):
-        self.bowling.roll(self.STRIKE_POINTS)
-        self.assertEqual(self.STRIKE_POINTS, self.bowling.get_score())
+    def test_should_get_score_without_strikes(self):
+        # Frame 1.
+        self.bowling.roll(9)
+        self.bowling.roll(0)
+
+        # Frame 2.
+        self.bowling.roll(3)
+        self.bowling.roll(4)
+
+        # Frame 3.
+        self.bowling.roll(1)
+        self.bowling.roll(5)
+
+        # Frame 4.
+        self.bowling.roll(2)
+        self.bowling.roll(2)
+
+        # Frame 5.
+        self.bowling.roll(1)
+        self.bowling.roll(0)
+
+        # Frame 6.
+        self.bowling.roll(6)
+        self.bowling.roll(3)
+
+        # Frame 7.
+        self.bowling.roll(0)
+        self.bowling.roll(3)
+
+        # Frame 8.
+        self.bowling.roll(9)
+        self.bowling.roll(0)
+
+        # Frame 9.
+        self.bowling.roll(9)
+        self.bowling.roll(0)
+
+        # Frame 10.
+        self.bowling.roll(5)
+        self.bowling.roll(3)
+
+        self.assertEqual(65, self.bowling.get_score())
+
